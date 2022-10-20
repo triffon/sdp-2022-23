2f324c189433935b7c2ab95a9ac3113fcb400e4c
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2022-10-20 10:20:59 +0300

    Преобразуване в обратен полски запис чрез поток

---
+++ lectures/1/calculator/rpn_converter.hpp
@@ -0,0 +1,45 @@
+#ifndef __RPN_CONVERTER
+#define __RPN_CONVERTER
+#include <iostream>
+#include "lstack.hpp"
+
+class RPNConverterStreamBuffer : public std::streambuf {
+private:
+    std::ostream& os;
+    LinkedStack<char> operationStack;
+
+public:
+    RPNConverterStreamBuffer(std::ostream& _os) : os(_os) {}
+
+    int_type overflow(int_type c) {
+        if (c == '=') {
+            while (!operationStack.empty())
+                os.put(operationStack.pop());
+        } else if (Calculator::isdigit(c))
+            os.put(c);
+        else if (Calculator::isop(c) || c == '(') {
+            char op;
+            while (!operationStack.empty() &&
+                   operationStack.peek() != '(' &&
+                   Calculator::priority(operationStack.peek()) >= Calculator::priority(c))
+                os.put(operationStack.pop());
+            operationStack.push(c);
+        }
+        else if (c == ')') {
+            char op;
+            while ((op = operationStack.pop()) != '(')
+                os.put(op);
+        } else 
+            throw std::runtime_error("Некоректен израз: непознат символ!");
+        return 0;
+    }
+};
+
+class RPNConverter : public std::ostream {
+private:
+    RPNConverterStreamBuffer sbuf;
+public:
+    RPNConverter(std::ostream& os) : sbuf(os), std::ostream(&sbuf) {}
+};
+
+#endif
