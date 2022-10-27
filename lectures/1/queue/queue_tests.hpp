c8e2e8b1a821a376c0168a7dd526e9b2f046055f
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2022-10-27 11:35:47 +0300

    Статична последователна реализация на опашка

--- lectures/1/queue/queue_tests.hpp
+++ lectures/1/queue/queue_tests.hpp
@@ -1,40 +1,44 @@
 #include "doctest.h"
 
+#include "squeue.hpp"
+
+TYPE_TO_STRING(StaticQueue<int>);
+
 TEST_CASE_TEMPLATE("След създаване на опашка, тя е празна",
-                   AnyQueue) {
+                   AnyQueue, StaticQueue<int>) {
     AnyQueue q;
     CHECK(q.empty());
 }
 
 TEST_CASE_TEMPLATE("След добавяне на елемент в опашка, тя не е празна",
-                   AnyQueue) {
+                   AnyQueue, StaticQueue<int>) {
     AnyQueue q;
     q.enqueue(42);
     CHECK_FALSE(q.empty());
 }
 
 TEST_CASE_TEMPLATE("Опит за поглеждане в или изключване на елемент от празна опашка хвърля изключение",
-                   AnyQueue) {
+                   AnyQueue, StaticQueue<int>) {
     AnyQueue q;
     CHECK_THROWS(q.head());
     CHECK_THROWS(q.dequeue());
 }
 
 TEST_CASE_TEMPLATE("При последователно добавяне на елементи в опашката се изключва първият добавен",
-                    AnyQueue) {
+                    AnyQueue, StaticQueue<int>) {
     AnyQueue q;
     for(int i = 1; i <= 10; i++)
-        q.push(i);
-    CHECK_EQUALS(q.head(), 1);
-    CHECK_EQUALS(q.dequeue(), 1);
-    CHECK_NOT_EQUALS(q.head(), 1);
+        q.enqueue(i);
+    CHECK_EQ(q.head(), 1);
+    CHECK_EQ(q.dequeue(), 1);
+    CHECK_NE(q.head(), 1);
 }
 
 TEST_CASE_TEMPLATE("При последователно добавяне на елементи в опашката след изключване на първия добавен, останалите остават в опашката",
-                    AnyQueue) {
+                    AnyQueue, StaticQueue<int>) {
     AnyQueue q;
     for(int i = 1; i <= 10; i++)
-        q.push(i);
+        q.enqueue(i);
     for(int i = 1; i <= 10; i++)
-        CHECK_EQUALS(q.dequeue(), i);
+        CHECK_EQ(q.dequeue(), i);
 }
