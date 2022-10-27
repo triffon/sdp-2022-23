5f1ad7d91d4dd62b6d046c815fcad13870c958dc
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2022-10-27 10:42:35 +0300

    Реализация на намиране на път чрез стекови рамки

--- lectures/1/horse/stack_horsewalker.cpp
+++ lectures/1/horse/stack_horsewalker.cpp
@@ -4,30 +4,44 @@
 StackHorseWalker::StackHorseWalker(size_t size) : HorseWalker(size) {}
 
 HorseWalk const& StackHorseWalker::findWalk(Position const& start, Position const& end) {
-    return hwalk;
-    /*
-    walk.push(start);
-    while(!walk.empty()) {
-        Position current = walk.peek();
-        if (current == end)
-            // успех
-            return true;
-
-        if (!insideBoard(current) || !insideBoard(end) || board[current.first][current.second])
-            // стъпка назад
-            walk.pop();
+    StackFrame startFrame;
+    startFrame.push(start);
+    stack.push(startFrame);
+    while(!stack.empty()) {
+        StackFrame& currentFrame = stack.peek();
+        if (currentFrame.empty())
+            stack.pop();
         else {
-            // стъпка напред
-            board[current.first][current.second] = true;
-            for(int dx = -2; dx <= 2; dx++)
-                if (dx != 0)
-                    for (int sign : {-1, 1}) {
-                        int dy = sign * (3 - std::abs(dx));
-                        walk.push({current.first + dx, current.second + dy});
-                    }
+            Position current = currentFrame.peek();
+            if (current == end) {
+                // успех
+                StackFrame reversedWalk;
+                while (!stack.empty())
+                    reversedWalk.push(stack.pop().peek());
+                while (!reversedWalk.empty())
+                    walk.push_back(reversedWalk.pop());
+                return walk;
+            }
+
+            if (!insideBoard(current) || !insideBoard(end) || board[current.first][current.second])
+                // стъпка назад
+                currentFrame.pop();
+            else {
+                // стъпка напред
+                // правим си нова стекова рамка
+                StackFrame newFrame;
+                board[current.first][current.second] = true;
+                for(int dx = -2; dx <= 2; dx++)
+                    if (dx != 0)
+                        for (int sign : {-1, 1}) {
+                            int dy = sign * (3 - std::abs(dx));
+                            Position newPosition{current.first + dx, current.second + dy};
+                            newFrame.push(newPosition);
+                        }
+                stack.push(newFrame);
+            }
         }
     }
 
-    return false;
-    */
+    return walk;
 }
--- lectures/1/horse/stack_horsewalker.hpp
+++ lectures/1/horse/stack_horsewalker.hpp
@@ -3,12 +3,13 @@
 #include "lstack.hpp"
 #include "horsewalker.hpp"
 
-using Walk = LinkedStack<Position>;
+using StackFrame = LinkedStack<Position>;
+using HorseStack = LinkedStack<StackFrame>;
 
 class StackHorseWalker : public HorseWalker {
 private:
-    HorseWalk hwalk;
-    Walk walk;
+    HorseStack stack;
+    HorseWalk walk;
 public:
     StackHorseWalker(size_t size = 8);
     HorseWalk const& findWalk(Position const& start, Position const& end);
