c58f17c50fb8a48f1027c4976919e81ab47c57b5
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2022-11-10 10:54:59 +0200

    Първи прост тест

--- lectures/1/list/list_tests.hpp
+++ lectures/1/list/list_tests.hpp
@@ -1,1 +1,11 @@
 #include "doctest.h"
+#include "llist.hpp"
+
+TYPE_TO_STRING(LinkedList<int>);
+#define ALL_LISTS LinkedList<int>
+
+TEST_CASE_TEMPLATE("При създаване на списък той е празен",
+                    AnyList, ALL_LISTS) {
+    AnyList l;
+    CHECK(l.empty());
+}
