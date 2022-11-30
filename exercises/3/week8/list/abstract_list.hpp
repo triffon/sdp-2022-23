4b27bf2b0a8051c36878e813898a6f82b13338c8
Stef Vartolomeev <stef.vartolomeev@gmail.com>
2022-11-30 14:07:58 +0200

    Group 3, week 8

---
+++ exercises/3/week8/list/abstract_list.hpp
@@ -0,0 +1,156 @@
+#ifndef __ABSTRACT_LIST_HPP
+#define __ABSTRACT_LIST_HPP
+
+#include <stdexcept>
+
+template <typename T, typename ConcretePosition /* extends Position<T> */>
+class Position {
+public:
+    // проверка за валидност на позицията
+    virtual bool valid() const = 0;
+
+    // if (it) ...
+    // if (!it) ...
+
+    operator bool() const { return valid(); }
+    bool operator!() const { return !valid(); } 
+
+    // константен достъп до елемента на позицията
+    virtual T const& get() const = 0;
+
+    // достъп до елемента на позицията с възможност за промяна
+    virtual T& get() = 0;
+
+    // следваща позиция
+    virtual ConcretePosition next() const = 0;
+
+    // предишна позиция
+    virtual ConcretePosition prev() const = 0;
+
+    // преместваме се на следващата позиция
+    // ++it
+    ConcretePosition& operator++() {
+        return (ConcretePosition&)(*this) = next();
+    }
+
+    // it++
+    ConcretePosition operator++(int) {
+        ConcretePosition save = (ConcretePosition&)*this;
+        ++*this;
+        return save;
+    }
+
+    // операции за сравнение
+    virtual bool operator==(Position const& pos) const = 0;
+    bool operator!=(Position const& pos) { return !(*this == pos); }
+
+    T const& operator*() const { return get(); } 
+    T&       operator*()       { return get(); } 
+};
+
+template <typename T, typename P /* extends Position<T> */>
+class AbstractList {
+protected:
+    void erase() {
+        T x;
+        while (!empty())
+            deleteFirst(x);
+    }
+
+public:
+    virtual bool empty() const { return !begin().valid(); }
+
+    // включване на първи елемент
+    virtual bool insertFirst(T const& x) { return insertBefore(x, begin()); }
+
+    // включване на последен елемент
+    virtual bool insertLast(T const& x) { return insertAfter(x, last()); }
+
+    // включване на елемент преди дадена позиция 
+    virtual bool insertBefore(T const& x, P const& pos) = 0;
+
+    // включване на елемент след дадена позиция 
+    virtual bool insertAfter(T const& x, P const& pos) = 0;  
+
+    // изключване на първи елемент
+    virtual bool deleteFirst(T& x) {
+        P pos = begin();
+        return deleteAt(x, pos);
+    }
+
+    // изключване на последен елемент
+    virtual bool deleteLast(T& x) {
+        P pos = last();
+        return deleteAt(x, pos);
+    }
+
+    // изключване на елемент преди дадена позиция 
+    virtual bool deleteBefore(T& x, P const& pos) = 0;  
+
+    // изключване на елемент на дадена позиция, унищавайки позицията 
+    virtual bool deleteAt(T& x, P& pos) = 0;  
+
+    // изключване на елемент след дадена позиция 
+    virtual bool deleteAfter(T& x, P const& pos) = 0;  
+
+    // константен достъп до елемент на дадена позиция
+    virtual T const& getAt(P const& pos) const {
+        if (!pos.valid())
+            throw std::runtime_error("Опит за достъп през невалидна позиция!");
+        return pos.get();
+    }
+
+    // достъп до елемент на дадена позиция с възможност за промяна
+    virtual T& getAt(P& pos) {
+        if (!pos.valid())
+            throw std::runtime_error("Опит за достъп през невалидна позиция!");
+        return pos.get();
+    }
+
+    // !!! пробив във класа, можем да променяме елементите на константен
+    // списък
+    // TODO: да се реализира ConstPosition
+
+    // връща позицията в началото
+    virtual P begin() const = 0;
+
+    // връща позицията в края
+    virtual P last() const = 0;
+
+    // връща позцията СЛЕД края
+    // [begin; end)
+    virtual P end() const = 0;
+
+    // конкатениране на списък на края на дадения
+    // O(|other|)
+    void append(AbstractList const& other) {
+        for(T const& x : other)
+            insertLast(x);
+    }
+
+    void reverse() {
+        T x;
+        /*
+        P originalFront = begin();
+        // O(n²)
+        while (originalFront != last()) {
+            // O(n)
+            deleteLast(x);
+            // O(n)
+            insertBefore(x, originalFront);
+        }
+        */
+        P originalBack = last();
+        // O(n) по памет и по време
+        while (originalBack != begin()) {
+            // O(1)
+            deleteFirst(x);
+            // O(1)
+            insertAfter(x, originalBack);
+        }
+    }
+
+    virtual ~AbstractList() {}
+};
+
+#endif
---
+++ exercises/3/week8/main.cpp
@@ -0,0 +1,150 @@
+#include <iostream>
+#include <vector>
+#include "stack/lstack.hpp"
+#include "queue/squeue.hpp"
+#include "list/llist.hpp"
+
+LinkedList<int> read(int n)
+{
+    LinkedList<int> result;
+    std::cout << "type in " << n << " numbers: ";
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        std::cin >> x;
+        result.insertLast(x); /// !!!
+    }
+    return result;
+}
+
+void print(LinkedList<int> &list)
+{
+    LinkedList<int>::I iter = list.begin(); // !!!!
+    while (iter != list.end())              // !!!!
+    {
+        std::cout << *iter << " "; // !!!!
+        iter++;                    // !!!!
+    }
+    std::cout << std::endl;
+
+    for (LinkedList<int>::I iter = list.begin(); iter != list.end(); iter++)
+    {
+        std::cout << *iter << " "; // !!!!
+    }
+    std::cout << std::endl;
+}
+
+template <typename T>
+LinkedList<LinkedList<T>> breakToGroups(LinkedList<T> list, int maxGroupSize)
+{
+    LinkedList<LinkedList<T>> groups;
+    int counter = 0;
+    for (LinkedList<int>::I iter = list.begin(); iter != list.end(); iter++)
+    {
+        if (0 == counter % maxGroupSize)
+        {
+            LinkedList<T> newGroup;
+            groups.insertLast(newGroup);
+        }
+        groups.last().get().insertLast(*iter);
+        counter++;
+    }
+    return groups;
+}
+
+int listToNumber(LinkedList<int> list)
+{
+    int number = 0;
+    for (LinkedList<int>::I iter = list.begin(); iter != list.end(); iter++)
+    {
+        number *= 10;
+        number += *iter;
+    }
+    return number;
+}
+
+LinkedList<int> numberToList(int n)
+{
+    LinkedList<int> list;
+    while (n != 0)
+    {
+        list.insertFirst(n % 10);
+        n /= 10;
+    }
+    if (list.empty())
+    {
+        list.insertFirst(0);
+    }
+    return list;
+}
+
+LinkedList<int> sumNumbers(LinkedList<int> n1, LinkedList<int> n2)
+{
+    return numberToList(listToNumber(n1) + listToNumber(n2));
+}
+
+LinkedList<int> sumNumbersV2(LinkedList<int> n1, LinkedList<int> n2)
+{
+    n1.reverse();
+    n2.reverse();
+
+    LinkedList<int> n;
+    LinkedList<int>::I i1 = n1.begin();
+    LinkedList<int>::I i2 = n2.begin();
+    int inHand = 0; // едно на ум
+    while (i1 != n1.end() && i2 != n2.end())
+    {
+        n.insertFirst((*i1 + *i2 + inHand) % 10);
+        inHand = (*i1 + *i2 + inHand) / 10;
+
+        i1++;
+        i2++;
+    }
+    while (i1 != n1.end())
+    {
+        n.insertFirst((*i1 + inHand) % 10);
+        inHand = (*i1 + inHand) / 10;
+        i1++;
+    }
+    while (i2 != n2.end())
+    {
+        n.insertFirst((*i2 + inHand) % 10);
+        inHand = (*i2 + inHand) / 10;
+        i2++;
+    }
+    if (inHand > 0)
+    {
+        n.insertFirst(inHand);
+    }
+    return n;
+}
+
+template <typename T>
+bool equals(const LinkedList<T> &l1, const LinkedList<T> &l2)
+{
+    LinkedList<int>::I i1 = l1.begin();
+    LinkedList<int>::I i2 = l2.begin();
+    while (i1 != l1.end() && i2 != l2.end())
+    {
+        if (*i1 != *i2)
+        {
+            return false;
+        }
+    }
+    return (i1 == l1.end() && i2 == l2.end();
+}
+
+template <typename T>
+bool isPalindrome(LinkedList<T> word)
+{
+    LinkedList<T> reversedWord = word;
+    reversedWord.reverse();
+    return equals(reversedWord, word);
+}
+
+int main()
+{
+    LinkedList<int> list = read(5);
+    print(list);
+    return 0;
+}
