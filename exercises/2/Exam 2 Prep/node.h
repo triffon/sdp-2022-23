58b1b4f29140e267b3d3acacacc5e39904cca240
dimbata23 <adimitrov23@gmail.com>
2022-12-23 02:58:33 +0200

    added exam prep 2

---
+++ exercises/2/Exam 2 Prep/node.h
@@ -0,0 +1,48 @@
+#ifndef __NODE_H
+#define __NODE_H
+#pragma once
+
+#include <string>
+#include <sstream>
+
+template <typename T>
+struct Node
+{
+  T     fData   = T();
+  Node* fpLeft  = nullptr;
+  Node* fpRight = nullptr;
+};
+
+template <typename T>
+std::string printTree(const Node<T>* pTree)
+{
+    std::stringstream sout;
+    if ( pTree != nullptr )
+    {
+        sout << '(';
+        sout << pTree->data;
+        sout << printTree( pTree->left );
+        sout << printTree( pTree->right );
+        sout << ')';
+    }
+    else
+    {
+        sout << '.';
+    }
+
+  return sout.str();
+}
+
+template <typename T>
+void eraseTree(Node<T>*& pTree)
+{
+    if ( pTree != nullptr )
+    {
+        eraseTree( pTree->left );
+        eraseTree( pTree->right );
+        delete pTree;
+        pTree = nullptr;
+    }
+}
+
+#endif
