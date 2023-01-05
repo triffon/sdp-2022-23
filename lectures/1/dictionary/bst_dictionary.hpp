665af0502d328e1c943704c1afab880624418863
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2023-01-05 12:09:28 +0200

    Довършване на реализацията на речник с двоично дърво за търсене

--- lectures/1/dictionary/bst_dictionary.hpp
+++ lectures/1/dictionary/bst_dictionary.hpp
@@ -7,38 +7,69 @@
 
 template <typename K, typename V>
 class BSTDictionary : public Dictionary<K, V>, BinSearchTree<KeyValuePair<K,V>> {
+private:
+    using KVP = KeyValuePair<K, V>;
+    using P = BinTreePosition<KVP>;
+    void collectKeys  (P pos, LinkedList<K>& keys) const {
+        if (pos) {
+            keys.insertLast((*pos).key);
+            collectKeys(-pos, keys);
+            collectKeys(+pos, keys);
+        }
+    }
+    void collectValues(P pos, LinkedList<V>& values) const {
+        if (pos) {
+            values.insertLast((*pos).value);
+            collectValues(-pos, values);
+            collectValues(+pos, values);
+        }
+
+    }
 public:
 
-    using KVP = KeyValuePair<K, V>;
     using BinSearchTree<KVP>::search;
+    using BinSearchTree<KVP>::insert;
+    using BinSearchTree<KVP>::root;
 
     // търсене на стойност по ключ
     V* lookup(K const& key) {
-        return nullptr;
+        P pos = search(key);
+        if (!pos)
+            return nullptr;
+        return &(*pos).value;
     }
 
     V const* lookup(K const& key) const {
-        return nullptr;
+        P pos = search(key);
+        if (!pos)
+            return nullptr;
+        return &(*pos).value;
     }
 
     // свързване на ключ със стойност
     bool add(K const& key, V const& value) {
-        return false;
+        return insert(KVP(key, value));
     }
 
     // изтриване на стойност, свързана с ключ
     bool remove(K const& key) {
-        return false;
+        //std::ofstream of("bstdict.dot");
+        //BinSearchTree<KVP>::DOTprint(of);
+        return BinSearchTree<KVP>::remove(key);
     }
 
     // списък от ключове
     LinkedList<K> keys() const {
-        return LinkedList<K>();
+        LinkedList<K> result;
+        collectKeys(root(), result);
+        return result;
     }
 
     // списък от стойности
     LinkedList<V> values() const {
-        return LinkedList<V>();
+        LinkedList<V> result;
+        collectValues(root(), result);
+        return result;
     }
 
 };
