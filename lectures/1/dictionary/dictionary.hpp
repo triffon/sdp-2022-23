e579585999486180493fe2446a4cab8b2edfdc89
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2023-01-05 10:28:10 +0200

    АТД речник

---
+++ lectures/1/dictionary/dictionary.hpp
@@ -0,0 +1,37 @@
+#ifndef __DICTIONARY_HPP
+#define __DICTIONARY_HPP
+
+#include "llist.hpp"
+
+template <typename K, typename V>
+class Dictionary {
+public:
+    // търсене на стойност по ключ
+    virtual V* lookup(K const& key) = 0;
+    virtual V const* lookup(K const& key) const = 0;
+
+    // свързване на ключ със стойност
+    virtual bool add(K const& key, V const& value) = 0;
+
+    // промяна на стойност, свързана с ключ
+    void set(K const& key, V const& newValue) {
+        V* pValue = lookup(key);
+        if (pValue)
+            *pValue = newValue;
+        else
+            add(key, newValue);
+    }
+
+    // изтриване на стойност, свързана с ключ
+    virtual bool remove(K const& key) = 0;
+
+    // списък от ключове
+    virtual LinkedList<K> keys() const = 0;
+
+    // списък от стойности
+    virtual LinkedList<V> values() const = 0;
+
+    virtual ~Dictionary() {}
+};
+
+#endif
